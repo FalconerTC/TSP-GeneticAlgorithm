cbe412850793e3cc6ca0e03213fe7b0b7c9e1e2f
Falcon Taylor-Carter <falconertc@colorado.edu>
2016-03-23 18:25:10 -0600

    Lint tour

--- Tour.py
+++ Tour.py
@@ -1,46 +1,51 @@
+"""Tour.py"""
 import random
 import math
 
 class Tour(object):
+    """Represents a single path through all given nodes"""
 
     def __init__(self, lx, ly):
-        self.lx = lx
-        self.ly = ly
+        self.x_points = lx
+        self.y_points = ly
+        self.cities = []
         self.distance = 0
         self.fitness = 0
-        self.cities = random.sample(range(len(lx)), len(lx))
-        #print "Cities: ",self.cities
+        self.set_tour()
 
-    def set_tour(self, l):
-        self.cities = l
+    def set_tour(self, city_list=None):
+        """Set the ordered list of cities that represents this tour"""
+        self.cities = city_list or \
+            random.sample(range(len(self.x_points)), len(self.y_points))
         self.distance = 0
         self.fitness = 0
-    
+
     def get_fitness(self):
+        """Calculate and retreive fitness"""
         if self.fitness == 0:
             self.fitness = 1 / self.get_cost()
         return self.fitness
 
-    def swap(self,index1,index2):
+    def swap(self, index1, index2):
+        """Swap two cities"""
         temp = self.cities[index1]
         self.cities[index1] = self.cities[index2]
         self.cities[index2] = temp
+        # Reset distance calculation
         self.distance = 0
+        self.fitness = 0
 
-    # Calculate total distance of tour
     def get_cost(self):
+        """Calculate total distance of tour"""
         if self.distance == 0:
             for i in range(1, len(self.cities) + 1):
-                p1 = self.cities[i-1]
-                p2 = self.cities[i % len(self.cities)]
-                self.distance += self.distance_to(p1, p2)
+                point1 = self.cities[i-1]
+                point2 = self.cities[i % len(self.cities)]
+                self.distance += self.distance_to(point1, point2)
         return self.distance
 
-    # Distance formula between two points
-    def distance_to(self, p1, p2):
-        xd = self.lx[p1] - self.lx[p2]
-        yd = self.ly[p1] - self.ly[p2]
-        return math.sqrt(xd*xd + yd*yd)
-
-
-    pass
+    def distance_to(self, point1, point2):
+        """Distance formula between two points"""
+        delta_x = self.x_points[point1] - self.x_points[point2]
+        delta_y = self.y_points[point1] - self.y_points[point2]
+        return math.sqrt(delta_x*delta_x + delta_y*delta_y)
